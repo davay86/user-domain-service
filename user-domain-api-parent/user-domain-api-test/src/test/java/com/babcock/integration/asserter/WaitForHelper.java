2e465ca0a65fff95dc85b6f764230ac6483ad5bd
SleepingTalent <jaybono30@hotmail.com>
2017-11-13 14:07:42 +0000

    updated repl

--- user-domain-api-parent/user-domain-api-test/src/test/java/com/babcock/integration/asserter/WaitForHelper.java
+++ user-domain-api-parent/user-domain-api-test/src/test/java/com/babcock/integration/asserter/WaitForHelper.java
@@ -1,5 +1,6 @@
 package com.babcock.integration.asserter;
 
+import com.noveria.assertion.exception.WaitUntilAssertionError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
